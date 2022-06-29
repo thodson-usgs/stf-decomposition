2d2f414c0f2e1acee2c8a1fc3d3d3d4f910471f7
kmiles1 <kmiles@usgs.gov>
2022-06-29 15:47:06 -0500

    Add test for inferring period from data

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,3 +1,4 @@
+from ast import parse
 import numpy as np
 import unittest
 import pandas as pd
@@ -7,14 +8,19 @@ sys.path.append('src')
 
 from stf_decomposition.stf_decomposition import stf_decomposition
 
-DATA_SOURCE = "tests/data/data.txt"
-
 class TestConstructor(unittest.TestCase):
     # stf_decomposition expects a data frame with an index
     # This will test if a correct error is thrown when a matrix is input instead of a data frame 
-    def test_data_input(self):
+    def test_incorrect_data_input(self):
         matrix = np.ones((10, 2))
         with self.assertRaises(ValueError):
             stf_decomposition(matrix, "blackman")
+    # This will test if no error is thrown when data with a period is input to stf_decomposition() without a formal period argument 
+    def test_no_period_input(self):
+        data = pd.read_csv("tests/data/co2.csv", index_col='date', parse_dates=True)
+        try:
+            stf_decomposition(data, "blackman")
+        except ValueError:
+            self.fail("stf_decomposition() did not correctly infer period from data")
 
         
